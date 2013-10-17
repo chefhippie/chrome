d03948dade620e9f1550a031db7cdf3ba2671b5a
Thomas Boerger <tboerger@tbpro.de>
2013-10-17 11:55:55 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "chrome"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures chrome"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "chrome", "Installs/Configures chrome"
+
+supports "suse"
